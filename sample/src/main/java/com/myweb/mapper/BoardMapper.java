b8ce3cdf9869c104770326a9796602b5c891e0fe
yi <yi@175.210.42.225>
2023-12-18 16:09:46 +0900

    create

---
+++ sample/src/main/java/com/myweb/mapper/BoardMapper.java
@@ -0,0 +1,22 @@
+package com.myweb.mapper;
+
+import java.util.List;
+
+import com.myweb.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	//xml에서 sql문을 처리했기때문에 필요 없음
+	//@Select("select * from tbl_board where bno > 0")
+	public List<BoardVO> getList();
+
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+}
---
+++ sample/src/main/java/com/myweb/service/BoardService.java
@@ -0,0 +1,20 @@
+package com.myweb.service;
+
+import java.util.List;
+
+import com.myweb.domain.BoardVO;
+
+public interface BoardService {
+
+	
+	public void register(BoardVO board);
+	
+	public BoardVO get(Long bno);
+	
+	public boolean modify(BoardVO board);
+	
+	public boolean remove(Long bno);
+	
+	public List<BoardVO> getList();
+
+}
