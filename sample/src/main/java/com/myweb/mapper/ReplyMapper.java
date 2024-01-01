f86a1cc51e68c1c65a324ee0ea455833908cbf27
yi <yi@175.210.42.225>
2024-01-01 16:35:58 +0900

    reply - CRUD

--- sample/src/main/java/com/myweb/mapper/ReplyMapper.java
+++ sample/src/main/java/com/myweb/mapper/ReplyMapper.java
@@ -1,6 +1,14 @@
 package com.myweb.mapper;
 
+import com.myweb.domain.ReplyVO;
+
 public interface ReplyMapper {
 
+	public int insert(ReplyVO vo);
 	
+	public ReplyVO read(Long rno);
+	
+	public int delete(Long rno);
+	
+	public int update(ReplyVO reply);
 }
--- sample/src/test/java/com/myweb/mapper/ReplyMapperTests.java
+++ sample/src/test/java/com/myweb/mapper/ReplyMapperTests.java
@@ -1,10 +1,14 @@
 package com.myweb.mapper;
 
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.myweb.domain.ReplyVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -14,11 +18,53 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ReplyMapperTests {
 
+	private Long[] bnoArr = {1572992L, 1572991L, 1572990L};
+	
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
 	
 	@Test
 	public void testMapper() {
 		log.info(mapper);
+	}
+	
+	@Test
+	public void testCreate() {
+		IntStream.rangeClosed(1, 10).forEach(i ->{
+			ReplyVO vo = new ReplyVO();
+			
+			//게시물 번호
+			vo.setBno(bnoArr[i % 3]);
+			vo.setReply("댓글 test " + i);
+			vo.setReplyer("replyer " + i);
+			
+			mapper.insert(vo);
+		});
+	}
+	
+	@Test
+	public void testRead() {
+		Long targetRno = 5L;
+		ReplyVO vo = mapper.read(targetRno);
+		log.info(vo);
+	}
+	
+	@Test
+	public void testDelete() {
+		Long targetRno = 1L;
+		mapper.delete(targetRno);
+	}
+	
+	@Test
+	public void testUpdate() {
+		Long targetRno = 10L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		
+		vo.setReply("Update Reply ");
+		
+		int count = mapper.update(vo);
+		
+		log.info("UPDATE COUNT: " + count);
 	}
 }
