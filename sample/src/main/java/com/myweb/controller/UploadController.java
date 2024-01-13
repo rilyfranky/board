03679767579bca99b895d754d027691d9c596133
yi <yi@175.210.42.225>
2024-01-13 11:24:44 +0900

    upload - 경로, 파일명

--- sample/src/main/java/com/myweb/controller/UploadController.java
+++ sample/src/main/java/com/myweb/controller/UploadController.java
@@ -1,6 +1,9 @@
 package com.myweb.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -45,11 +48,29 @@ public class UploadController {
 		log.info("upload ajax");
 	}
 	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+		
+		return str.replace("-", File.separator);
+	}
+	
 	@PostMapping("/uploadAjaxAction")
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
 		log.info("update ajax post......");
 		
 		String uploadFolder = "C:\\upload";
+		
+		//make folder
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path: " + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
 		
 		for (MultipartFile multipartFile : uploadFile) {
 			
@@ -63,7 +84,12 @@ public class UploadController {
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 			log.info("only file name: " + uploadFileName);
 			
-			File saveFile = new File(uploadFolder, uploadFileName);
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			//File saveFile = new File(uploadFolder, uploadFileName);
+			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				multipartFile.transferTo(saveFile);
