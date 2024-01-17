d1e99064cbecad43ed043808fd385a0bf07e28d3
yi <yi@175.210.42.225>
2024-01-17 10:36:31 +0900

    upload - board 기능과 통합

---
+++ sample/src/main/java/com/myweb/mapper/BoardAttachMapper.java
@@ -0,0 +1,15 @@
+package com.myweb.mapper;
+
+import java.util.List;
+
+import com.myweb.domain.BoardAttachVO;
+
+public interface BoardAttachMapper {
+	
+	public void insert(BoardAttachVO vo);
+	
+	public void delete(String uuid);
+	
+	public List<BoardAttachVO> findByBno(Long bno);
+
+}
